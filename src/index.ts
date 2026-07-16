2cd4958e5457a00976464a730a55308ace302da4
Zimspace <hoangquansongcaum@gmail.com>
2026-07-16 09:38:59 +0700

    Export runtime storage registry from package root

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,5 @@ export * from './prefix-authorizer.js';
 export * from './profile-registry.js';
 export * from './runtime-contract.js';
 export * from './runtime-service.js';
+export * from './runtime-storage-registry.js';
 export * from './service.js';
